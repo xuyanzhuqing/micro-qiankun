fa02aa3ac90769928abd8b7b5ea4b99921e38609
michael <962275976@qq.com>
2023-10-17 18:30:25 +0800

    update preinstall script

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -5,6 +5,5 @@ pnpm install --filter ./packages/locale && pnpm build --filter ./packages/locale
 pnpm install --filter ./packages/axios && pnpm build --filter ./packages/axios &&
 pnpm install --filter ./packages/theme && pnpm build --filter ./packages/theme &&
 pnpm install --filter ./packages/components && pnpm build --filter ./packages/components &&
-
-pnpm install --filter ./apps/main && pnpm build --filter ./apps/main
+pnpm install --filter ./apps/main
 
