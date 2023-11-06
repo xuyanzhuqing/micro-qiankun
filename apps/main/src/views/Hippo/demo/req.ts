d2feabc118f540d21fc3aeeff602d90fe5107ca5
michael <962275976@qq.com>
2023-11-06 14:49:34 +0800

    尝试 page 组建

---
+++ apps/main/src/views/Hippo/demo/req.ts
@@ -0,0 +1,5 @@
+export interface DemoApiReq {
+  pageSize?: number;
+  current?: number;
+  keyword?: string;
+}
--- apps/main/src/views/Hippo/index.tsx
+++ apps/main/src/views/Hippo/index.tsx
@@ -1,186 +1,225 @@
-import { DownOutlined } from '@ant-design/icons';
-import type {
-  ProColumnType,
-  ProFormInstance,
-  ProTableProps,
-} from '@ant-design/pro-components';
-import {
-  ProCard,
-  ProTable,
-} from '@ant-design/pro-components';
-import { Button } from 'antd';
-import { useRef, useState } from 'react';
+import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
+import { ProTable, TableDropdown } from '@ant-design/pro-components';
+import { Button, Dropdown, Space, Tag } from 'antd';
+import { DemoApiReq, DemoApiRes, GithubIssueItem } from './demo';
+import { useRef } from 'react';
 
-type DataType = {
-  age: number;
-  address: string;
-  name: string;
-  time: number;
-  key: number;
-  description: string;
+export const getRoutesApiSwr = (params: DemoApiReq): Promise<DemoApiRes> => fetch(
+  `https://proapi.azurewebsites.net/github/issues?curent=${params.current}&pageSize=${params.pageSize}`,
+).then(res => res.json())
+
+export const waitTimePromise = async (time: number = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
 };
 
-const columns: ProColumnType<DataType>[] = [
+export const waitTime = async (time: number = 100) => {
+  await waitTimePromise(time);
+};
+
+const columns: ProColumns<GithubIssueItem>[] = [
   {
-    title: 'Name',
-    dataIndex: 'name',
+    dataIndex: 'index',
+    valueType: 'indexBorder',
+    width: 48,
   },
   {
-    title: 'time',
-    dataIndex: 'time',
-    valueType: 'time',
+    title: '标题',
+    dataIndex: 'title',
+    copyable: true,
+    ellipsis: true,
+    tip: '标题过长会自动收缩',
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    valueType: 'select',
+    disable: true,
+    title: '状态',
+    dataIndex: 'state',
     filters: true,
     onFilter: true,
+    ellipsis: true,
+    valueType: 'select',
     valueEnum: {
-      london: {
-        text: '伦敦',
+      all: { text: '超长'.repeat(50) },
+      open: {
+        text: '未解决',
+        status: 'Error',
       },
-      'New York': {
-        text: '纽约',
+      closed: {
+        text: '已解决',
+        status: 'Success',
+        disabled: true,
+      },
+      processing: {
+        text: '解决中',
+        status: 'Processing',
       },
     },
   },
   {
-    title: 'Action',
-    key: 'action',
+    disable: true,
+    title: '标签',
+    dataIndex: 'labels',
+    search: false,
+    renderFormItem: (_, { defaultRender }) => {
+      return defaultRender(_);
+    },
+    render: (_, record) => (
+      <Space>
+        {record.labels.map(({ name, color }) => (
+          <Tag color={color} key={name}>
+            {name}
+          </Tag>
+        ))}
+      </Space>
+    ),
+  },
+  {
+    title: '创建时间',
+    key: 'showTime',
+    dataIndex: 'created_at',
+    valueType: 'date',
     sorter: true,
+    hideInSearch: true,
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'created_at',
+    valueType: 'dateRange',
+    hideInTable: true,
+    search: {
+      transform: (value) => {
+        return {
+          startTime: value[0],
+          endTime: value[1],
+        };
+      },
+    },
+  },
+  {
+    title: '操作',
     valueType: 'option',
-    render: () => [
-      <a key="delete">Delete</a>,
-      <a key="link" className="ant-dropdown-link">
-        More actions <DownOutlined />
+    key: 'option',
+    render: (text, record, _, action) => [
+      <a
+        key="editable"
+        onClick={() => {
+          action?.startEditable?.(record.id);
+        }}
+      >
+        编辑
       </a>,
+      <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
+        查看
+      </a>,
+      <TableDropdown
+        key="actionGroup"
+        onSelect={() => action?.reload()}
+        menus={[
+          { key: 'copy', name: '复制' },
+          { key: 'delete', name: '删除' },
+        ]}
+      />,
     ],
   },
 ];
 
-const genData = (index: number, pageSize: number) => {
-  const data: DataType[] = [];
-  const start = index * pageSize
-  const end = start + pageSize
-  for (let i = start; i < end; i += 1) {
-    data.push({
-      key: i,
-      name: 'John Brown',
-      age: i + 10,
-      time: 1661136793649 + i * 1000,
-      address: i % 2 === 0 ? 'london' : 'New York',
-      description: `My name is John Brown, I am ${i}2 years old, living in New York No. ${i} Lake Park.`,
-    });
-  }
-  return data;
-};
-
-// 自定义 query string
-type Params = {
-  filter: string
-}
-
-const initData: ProTableProps<DataType, Params> = {
-  bordered: true,
-  loading: false,
-  columns,
-  pagination: {
-    pageSize: 10,
-    current: 1,
-    total: 100,
-  },
-  size: 'small',
-  headerTitle: '高级表格',
-  tooltip: '高级表格 tooltip',
-  showHeader: true,
-  rowSelection: {},
-  tableLayout: undefined,
-  search: {
-    span: 12,
-    labelWidth: 80,
-    filterType: 'query',
-    layout: 'horizontal',
-  },
-  options: {
-    density: true,
-    fullScreen: true,
-    setting: true,
-  },
-};
-
-const Hippo = () => {
-  const ref = useRef<ProFormInstance>();
-  const [config, setConfig] = useState(initData);
-  const tableColumns = (config.columns || columns)?.map((item: any) => ({
-    ...item,
-    ellipsis: true,
-  }));
-
-  const params: Params = {
-    filter: 'xxxx'
-  }
-
+export default () => {
+  const actionRef = useRef<ActionType>();
   return (
-    <ProCard
-      split="vertical"
-      bordered
-      headerBordered
-      style={{
-        height: '100vh',
-        overflow: 'hidden',
+    <ProTable<GithubIssueItem>
+      columns={columns}
+      actionRef={actionRef}
+      cardBordered
+      request={async (params, sort, filter) => {
+        console.log(sort, filter);
+        await waitTime(2000);
+        const data = await getRoutesApiSwr(params)
+        return data
       }}
-    >
-      <ProCard
-        style={{
-          height: '100vh',
-          overflow: 'auto',
-        }}
-      >
-        <ProTable
-          {...config}
-          locale={{ filterReset: '重置', filterConfirm: '确定' }}
-          formRef={ref}
-          pagination={config.pagination}
-          search={config.search}
-          expandable={
-            config.expandable && {
-              expandedRowRender: (record: DataType) => (
-                <p>{record.description}</p>
-              ),
-            }
-          }
-          options={config.options}
-          toolBarRender={
-            config?.toolBarRender
-              ? () => [
-                <Button key="refresh" type="primary">
-                  刷新
-                </Button>,
-              ]
-              : false
-          }
-          footer={config.footer}
-          headerTitle={config.headerTitle}
-          columns={tableColumns}
-          request={async (
-            // 第一个参数 params 查询表单和 params 参数的结合
-            // 第一个参数中一定会有 pageSize 和  current ，这两个参数是 antd 的规范
-            params,
-            sort,
-            filter,
-          ) => {
+      editable={{
+        type: 'multiple',
+      }}
+      columnsState={{
+        persistenceKey: 'pro-table-singe-demos',
+        persistenceType: 'localStorage',
+        onChange(value) {
+          console.log('value: ', value);
+        },
+      }}
+      rowKey="id"
+      search={{
+        labelWidth: 'auto',
+      }}
+      options={{
+        setting: {
+          listsHeight: 400,
+        },
+      }}
+      form={{
+        // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
+        syncToUrl: (values, type) => {
+          if (type === 'get') {
             return {
-              data: genData(params?.current ?? 0, params?.pageSize ?? 10),
-              success: true,
-              total: 100
-            }
+              ...values,
+              created_at: [values.startTime, values.endTime],
+            };
+          }
+          return values;
+        },
+      }}
+      pagination={{
+        pageSize: 5,
+        onChange: (page) => console.log(page),
+      }}
+      dateFormatter="string"
+      headerTitle="高级表格"
+      toolBarRender={() => [
+        <Button
+          key="button"
+          icon={<PlusOutlined />}
+          onClick={() => {
+            actionRef.current?.reload();
           }}
-          scroll={config.scroll}
-        />
-      </ProCard>
-    </ProCard>
+          type="primary"
+        >
+          新建
+        </Button>,
+        <Dropdown
+          key="menu"
+          menu={{
+            items: [
+              {
+                label: '1st item',
+                key: '1',
+              },
+              {
+                label: '2nd item',
+                key: '1',
+              },
+              {
+                label: '3rd item',
+                key: '1',
+              },
+            ],
+          }}
+        >
+          <Button>
+            <EllipsisOutlined />
+          </Button>
+        </Dropdown>,
+      ]}
+    />
   );
 };
-
-export default Hippo;
