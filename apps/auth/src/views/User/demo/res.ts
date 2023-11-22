a0c46f6ba97f513c1e1157dad491dcc9510f5290
michael <962275976@qq.com>
2023-11-22 10:35:06 +0800

    新增 user page

---
+++ apps/auth/src/views/User/demo/res.ts
@@ -0,0 +1,22 @@
+export type GithubIssueItem = {
+  url: string;
+  id: number;
+  number: number;
+  title: string;
+  labels: {
+    name: string;
+    color: string;
+  }[];
+  state: string;
+  comments: number;
+  created_at: string;
+  updated_at: string;
+  closed_at?: string;
+};
+
+export interface DemoApiRes {
+  page: number;
+  success: boolean;
+  total: number;
+  data: GithubIssueItem[]
+}
